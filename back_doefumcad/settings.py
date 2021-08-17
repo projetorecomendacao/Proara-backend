5ecc61965eb4a4e1a12fbe6db3dcaadf2291217c
Laurentino Dantas <laurentino.dantas@usp.br>
2021-08-17 11:29:41 -0300

    Primeiro Deploy

--- back_doefumcad/settings.py
+++ back_doefumcad/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -171,3 +173,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "statics")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
